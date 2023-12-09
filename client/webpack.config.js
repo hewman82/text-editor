ee1fbd0519e3635fba38893a1f3907268522cdd4
hewman82 <138827425+hewman82@users.noreply.github.com>
2023-12-09 13:30:30 -0800

    plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -18,12 +16,72 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text-Editor'
+      }),
+      new MiniCssExtractPlugin(),
+      new GenerateSW({
+        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+        // Define runtime caching rules.
+        runtimeCaching: [{
+          // Match any request that ends with .png, .jpg, .jpeg or .svg.
+          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+  
+          // Apply a cache-first strategy.
+          handler: 'CacheFirst',
+  
+          options: {
+            // Use a custom cache name.
+            cacheName: 'images',
+  
+            // Only cache 2 images.
+            expiration: {
+              maxEntries: 2,
+            },
+          },
+        }],
+      }),
+      new WebpackPwaManifest({
+        name: 'Text-Editor',
+        short_name: 'Text-Editor',
+        description: 'Create notes offline',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
